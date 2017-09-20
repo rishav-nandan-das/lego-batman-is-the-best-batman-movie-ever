74fb6784717a20be2071d664cb2732f0c9c76669
rishav-nandan-das <31542867+rishav-nandan-das@users.noreply.github.com>
2017-09-21 00:20:51 +0800

    Add files via upload

---
+++ xyz.js
@@ -0,0 +1,14 @@
+.container
+{
+margin:auto;
+width:50%;
+min-width:30%;
+border:20px solid white;
+background-color:gray;
+padding:20px;
+font-size:25px;
+}
+.loop2
+{font-family:gill sans;
+font-size:30px;
+}
